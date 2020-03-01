71f1d1b5832e7a9ac03d833e8d1a65b1bb4d94ed
Kenton Weber <webekent@oregonstate.edu>
2020-03-01 21:51:00 +1100

    Added circleArea function and tests

--- task.py
+++ task.py
@@ -1,2 +1,14 @@
+import math
+
+
 def firstrun():
     return "success"
+
+def circleArea(radius):
+    if radius < 0:
+        return None
+    elif radius == 0:
+        return 0
+    else:
+        Area = math.pi*radius*radius
+        return Area
--- tests.py
+++ tests.py
@@ -1,5 +1,7 @@
 import unittest
 import task
+import random
+import math
 
 
 class TestCase(unittest.TestCase):
@@ -11,6 +13,18 @@ class TestCase(unittest.TestCase):
     def test2(self):
         expected = "failure"
         self.assertNotEqual(expected, task.firstrun())
+
+    def testCircleArea(self):
+        expected = 0
+        self.assertEqual(expected, task.circleArea(0))
+        i = 0
+        while i < 10000:
+            r = random.uniform(-10.0, 10.0)
+            if r < 0:
+                expected = None
+            else:
+                expected = math.pi*r^2
+            self.assertEqual(expected, task.circleArea(r))
 
     if __name__ == '__main__':
         unittest.main()
