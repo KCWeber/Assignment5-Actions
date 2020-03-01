df7724c4be75ce3b4adbceab628a55c37211c698
Kenton Weber <webekent@oregonstate.edu>
2020-03-01 22:56:19 +1100

    added firstAndLast function and test

--- task.py
+++ task.py
@@ -13,3 +13,16 @@ def circleArea(radius):
     else:
         Area = math.pi*radius*radius
         return Area
+
+
+def firstAndLast(theList):
+    size = len(theList)
+    if size < 1:
+        return []
+    elif size == 1:
+        first = theList[0]
+        return [first, first]
+    else:
+        first = theList[0]
+        last = theList[size-1]
+        return [first, last]
--- tests.py
+++ tests.py
@@ -27,5 +27,19 @@ class TestCase(unittest.TestCase):
             self.assertAlmostEqual(expected, task.circleArea(r), places=8)
             i = i + 1
 
+    def testFirstLast(self):
+        list1 = []
+        expected = []
+        self.assertEqual(expected, task.firstAndLast(list1))
+        list1 = [123]
+        expected = [123, 123]
+        self.assertEqual(expected, task.firstAndLast(list1))
+        list1 = [111, 222]
+        expected = [111, 222]
+        self.assertEqual(expected, task.firstAndLast(list1))
+        list1 = [111, 222, 333, 99]
+        expected = [111, 99]
+        self.assertEqual(expected, task.firstAndLast(list1))
+
     if __name__ == '__main__':
         unittest.main()
